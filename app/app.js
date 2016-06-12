45301d6821fffd9a9bd49b8d4f053aafa0f2b670
S4NT14G0 <roigs23@gmail.com>
2016-06-11 20:47:31 -0400

    Added in project

---
+++ app/app.js
@@ -0,0 +1,10 @@
+﻿var MyApp = angular.module("MyApp", ["ngRoute", "ngResource", "appRoutes",
+    "ngMaterial", "ngMessages", "ngSanitize", "ngAnimate",'ngMessages'])
+    .config(function ($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+          .primaryPalette('light-blue')
+          .accentPalette('orange');
+        $mdThemingProvider.theme('docs-dark', 'default')
+            .primaryPalette('yellow')
+            .dark();
+    });
