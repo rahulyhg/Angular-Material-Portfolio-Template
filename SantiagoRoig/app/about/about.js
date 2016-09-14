659afbbae5d2803a9458df7c9332bfd0ecc9f465
S4NT14G0 <roigs23@gmail.com>
2016-09-14 15:13:09 -0400

    Worked on about page

--- SantiagoRoig/app/about/about.js
+++ SantiagoRoig/app/about/about.js
@@ -1,3 +1,12 @@
 ﻿MyApp.controller('AboutController', function ($scope) {
-    
+    $scope.contactMe = {
+        name: '',
+        email: '',
+        message: ''
+    };
+
+    $scope.sendEmail = function(){
+
+
+    }
 });
